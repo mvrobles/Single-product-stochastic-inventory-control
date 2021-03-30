830fe84234e94b377a0f95afa19c4756acb697a6
Melissa Robles <melissa.robles07@gmail.com>
2021-03-30 17:58:55 -0500

    VI

---
+++ Value Iteration.R
@@ -0,0 +1,118 @@
+
+#####################################################################
+
+#Calcula los valores de pj para j=0,1...200 y los guarda en un vector
+psubj = rep(0, 201)
+for( j in 1:201 ){
+  #psubj[1] = p_0
+  psubj[j] = dpois(x=(j-1), lambda = 10) 
+}
+
+#Calcula los valores de qj para j = 1...200 y los guarda en un vector
+qsubj = rep(0,201)
+for( j in 1:201 ){
+   qsubj[j] = 1 - ppois(q = (j-2), lambda = 10, lower.tail = TRUE)
+}
+
+#####################################################################
+
+#M, maximo stock
+M = 100
+
+#lambda
+lambda = 0.9
+
+recompensa <- function(s,a){
+  rta = 0
+  if(s + a -1 > 0){
+  for(j in 0:(s+a-1)){
+    rta = rta + (10*j*psubj[j+1])
+  }
+  rta = rta + (10*(s+a)*qsubj[s+a+1] - 3*a - s)
+  #Si a>0 se debe hacer el descuento de 2 que es el valor de realizar el pedido.
+  if(a > 0){
+    rta = rta - 2
+  }}
+  return(rta)
+}
+
+sumaVI <- function(s,a,v0){
+  rta = 0
+  for (j in 0:M){
+    pjsa = 0
+    if( j > 0 && j <= (s+a) && (s+a) <= 100){
+      pjsa = psubj[s+a-j+1]
+    }
+    else if( (s+a) <= M && j == 0 ){
+      pjsa = qsubj[s+a+1]
+    }
+    rta = rta + (pjsa*v0[j+1])
+  }
+  return(rta)
+}
+
+#Definimos v0(s) = 0 para todo s.
+#Esta variable la utilizaremos para las iteraciones de forma recursiva. Es vn.
+v0 = rep(0,M+1)
+
+#Este va a representar vn+1 cuando calculemos iterativamente.
+vn = rep(0,M+1)
+
+#epsilon lo tomamos como 0.1
+epsilon = 0.1
+
+#Inicializamos n como 0.
+n = 0
+
+parar = FALSE
+
+while(parar == FALSE){
+  print(n)
+  #Itero sobre los s
+  for( s in 0:M )
+  {
+    #Los valores a encontrar el máximo (r(s,a) + sum...)
+    valores <- rep(0, (M-s))
+    
+    #Comenzamos la iteración sobre A(i)
+    for( a in 0:(M-s) )
+    {
+      valores[a+1] = recompensa(s,a) + (0.9 * sumaVI(s,a,v0))
+    } 
+    
+    #Tomamos el maximo entre los valores calculados
+    vn[s] = max(valores)
+  }
+   
+    
+    #Condición de parada
+    norma = max(abs(vn-v0))
+    
+    if(norma < ((epsilon*(1-lambda))/(2*lambda))){
+      parar = TRUE
+    }
+    v0 <- vn
+    n = n+1
+  
+}
+
+#Terminó el paso 3 del algoritmo
+print("Terminó el tercer paso.")
+
+#Comienza el paso 4.
+d = rep(0,M+1)
+
+#Itero sobre los s
+for( s in 0:M )
+{
+  #Los valores a encontrar el máximo (r(s,a) + sum...)
+  valores = rep(0, (M-s))
+  
+  #Comenzamos la iteración sobre A(i)
+  for( a in 0:(M-s) ){
+    valores[a+1] = recompensa(s,a) + (0.9 * sumaVI(s,a,vn))
+  }
+  
+  d[s+1] = which.max(valores)-1
+
+}
